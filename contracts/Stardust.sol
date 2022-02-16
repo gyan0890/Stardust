4231509c2a35cf027e143cf590d1c93c4c042ea0
Gyan <gyanlakshmi@gmail.com>
2022-02-16 23:48:06 +0530

    Update Stardust.sol

--- contracts/Stardust.sol
+++ contracts/Stardust.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.0;
 
 contract Stardust {
-
+    uint256 maxFractions;
+    address owner;
     struct Collection {
         address collectionAddr;
         uint256 totalNft;
@@ -14,48 +15,51 @@ contract Stardust {
     }
 
     mapping(address => address) fractionContractMapping;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
         
     event Launch(address indexed, address indexed);
 
-    constructor() {
-         
+    constructor(uint256 _maxFractions) {
+         maxFractions = _maxFractions;
+         owner = msg.sender;
     }
 
     function addCollection(address collectionAddress, string memory tokenName, string memory tokenSymbol, uint256 _totalNft) public onlyOwner returns(bool) {
         require(collectionAddress != address(0), "Collection address is not valid");
-        Collection collection = new Collection(collectionAddress,_totalNft, 0, 0);
+        Collection memory collection = Collection(collectionAddress,_totalNft, 0, 0);
 
+        uint256 numberOfTokens = _totalNft*maxFractions;
+        FractionaliseStardust fractions = new FractionaliseStardust(tokenName, tokenSymbol, numberOfTokens);
 
+        fractionContractMapping[collectionAddress] = address(fractions);
 
+        return true;
 
     }
 
-    function deploy(string memory name, string memory symbol, string memory description, uint256 totalSupply) external returns (address) {
-        uint256 tokenSupply = totalSupply*(1 ether);
-        DAOLaunch dao = new DAOLaunch(name, symbol, description, tokenSupply, msg.sender);
-        Contracts.push();
+    function depositNftPool(address collectionAddress, uint256 tokenId) public returns(address) {
+        ERC721 nftAddress = ERC721(collectionAddress);
+        require(fractionContractMapping[collectionAddress] != address(0), "Collection is not registered");
+        require(msg.sender == nftAddress.ownerOf(tokenId), "Caller is not the owner.");
 
-        uint Index = Contracts.length - 1;
-        Contracts[Index].owner = msg.sender;
-        Contracts[Index].id = Index;
-        Contracts[Index].contractAddress = address(dao);
+        //Transferring the NFT into the contract - need an approval call before this
+        nftAddress.safeTransferFrom(msg.sender, address(this), tokenId);
 
+        FractionaliseStardust fractionalTokens =  FractionaliseStardust(fractionContractMapping[collectionAddress]);
 
-        addressContractMap[msg.sender].push(address(dao));
+        fractionalTokens.mintToken(maxFractions, msg.sender);
 
-        emit Launch(address(dao), msg.sender);
+        return address(fractionalTokens);
 
-        return address(dao);
     }
 
     //Returns the DAO contract addresses for a particular owner
-    function getContract() external view returns(address[] memory) {
-        return(addressContractMap[msg.sender]);
-    }
-
-    //Get all the contract addresses of the various DAOs
-    function getAllContracts() external view returns(_contract[] memory) {
-        return Contracts;
+    function getContract(address _collection) external view returns(address) {
+        return(fractionContractMapping[_collection]);
     }
 
 }
@@ -75,37 +79,20 @@ contract FractionaliseStardust is ERC20 {
 
     event Mint(address indexed, uint256 indexed);
 
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
-    }
-
     constructor(string memory _name, string memory _symbol, uint256 _maxSupply) ERC20(_name, _symbol) {
       maxSupply = _maxSupply;
       active = true;
     }
 
-    //Mint the tokens for a DAO - possible only once
-    function mintToken(uint256 numTokens) public onlyOwner {
+    //Mint the tokens for the NFT
+    function mintToken(uint256 numTokens, address to) public {
         require(maxSupply > 0, "Maximum Token Supply cannot be 0");
         require(owner != address(0), "Owner cannot be 0 address");
-        require(!mintState, "Tokens have already been minted");
+        require(tx.origin == to, "Origin is not owner");
 
-        _mint(owner, numTokens);
-        mintState = true;
-        emit Mint(msg.sender, maxSupply);
+        _mint(to, numTokens);
+        emit Mint(to, numTokens);
 
-    } 
-
-    function deactivate() public onlyOwner() {
-        active = false;
-    }
-
-    
-    //Return the metadata of a DAO
-    function getMetadata() external view returns(string memory, string memory, string memory, uint256, bool) {
-        return (daoName, daoSymbol, description, maxSupply, active);
     }
 
 }
